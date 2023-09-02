d49f45ad7b51689969036adb3feac1512662dd06
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-09-03 03:37:09 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import "../globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TheZen",
+  description: "TheZen Application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
